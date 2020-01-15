0ec05c697a93491b0f51ceb94ca970c1f9cdc18d
Eric Davenport <EricDavenport@pursuit.org>
2020-01-15 14:04:37 -0500

    country API completed - nt tested

---
+++ CountriesCollectionView/CountriesAPIClient.swift
@@ -0,0 +1,42 @@
+//
+//  CountriesAPIClient.swift
+//  CountriesCollectionView
+//
+//  Created by Eric Davenport on 1/14/20.
+//  Copyright © 2020 Eric Davenport. All rights reserved.
+//
+
+import Foundation
+import NetworkHelper
+import ImageKit
+
+struct CountriesAPIClient {
+  
+  static func getCountries(completion: @escaping (Result<[Country], AppError>) -> ()) {
+    
+    let endpointURLString = ""
+    
+    guard let url = URL(string: endpointURLString) else {
+      completion(.failure(.badURL(endpointURLString)))
+      return
+    }
+    
+    let request = URLRequest(url: url)
+    
+    NetworkHelper.shared.performDataTask(with: request) { (result) in
+      switch result {
+      case .failure(let appError):
+        completion(.failure(.networkClientError(appError)))
+      case .success(let data):
+        do {
+          let results = try JSONDecoder().decode([Country].self, from: data)
+          
+          completion(.success(results))
+        } catch {
+          completion(.failure(.decodingError(error)))
+        }
+      }
+    }
+  
+}
+}
---
+++ CountriesCollectionView/CountryInfo.swift
@@ -0,0 +1,24 @@
+//
+//  CountryInfo.swift
+//  CountriesCollectionView
+//
+//  Created by Eric Davenport on 1/15/20.
+//  Copyright © 2020 Eric Davenport. All rights reserved.
+//
+
+import Foundation
+
+struct Country: Codable {
+  let name: String
+  let capital: String
+  let region: String
+  let population: Int
+  let currencies: Currency
+  let aplha2Code: String
+}
+
+struct Currency: Codable {
+  let code: String
+  let name: String
+  let symbol: String
+}
